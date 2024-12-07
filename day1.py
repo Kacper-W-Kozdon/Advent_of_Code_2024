b9e0e79369b97d9f82ee3ae9969aedc5f9f71904
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-12-07 17:11:55 +0100

    day 1, adds file loading

---
+++ day1.py
@@ -0,0 +1,9 @@
+def load_files():
+    fContent = []
+    with open("input1.txt") as f:
+        
+        for (lineIndex, line) in enumerate(f):  #loading the file into an np.array
+            if bool(line) and line != "\n":
+                fContent.append(line.strip("\n"))
+
+    return(fContent)
