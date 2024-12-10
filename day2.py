f978dbffc57252071c50675d04c92657927869ce
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-12-10 14:55:37 +0100

    day2: task1 finished

--- day2.py
+++ day2.py
@@ -17,14 +17,14 @@ def load_files():
 @guvectorize(["void(int32[:, :], int64[:])"], "(m,n)->(m)")
 def task1(report: np.array, output: np.array):
 
-    def test(entry1: np.array, entry2: np.array):
+    def test1(entry1: np.array, entry2: np.array):
         return abs(entry2 - entry1) <= 3
 
     for entry_id, entry in enumerate(report):
-        test1 = int(len(list(entry)) ==  len(list(set(list(entry)))))
-        test2 = int(sorted(list(entry)) == list(entry) or sorted(list(entry), reverse=True) == list(entry))
+        entry_comparator = list(set(list(entry)))
+        test2 = int(list(entry) ==  entry_comparator or list(entry) == sorted(entry_comparator, reverse=True))
 
-        output[entry_id] = np.prod(np.array(list(map(test, entry[:-1], entry[1:])))) * test1 * test2
+        output[entry_id] = np.prod(np.array(list(map(test1, entry[:-1], entry[1:])))) * test2
 
 def main():
     report = load_files()
