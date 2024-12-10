1196e05b501874888b9312531e5013b93adb1c44
Kacper-W-Kozdon <k.kozdon@gmail.com>
2024-12-10 14:51:20 +0100

    day: task1 finished

--- day2.py
+++ day2.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def load_files():
     fContent1 = []
-    fContent2 = []
+
     with open("testinput2.txt") as f:
 
         for (lineIndex, line) in enumerate(f):  #loading the file into an np.array
@@ -14,13 +14,23 @@ def load_files():
 
     return(fContent1)
 
-@guvectorize(["void(int32[:, :], int64[:])"], "(m,n)->(n)")
+@guvectorize(["void(int32[:, :], int64[:])"], "(m,n)->(m)")
 def task1(report: np.array, output: np.array):
+
     def test(entry1: np.array, entry2: np.array):
         return abs(entry2 - entry1) <= 3
 
     for entry_id, entry in enumerate(report):
-        output[entry_id] = np.prod(np.array(list(map(test, entry[:-1], entry[1:]))))
+        test1 = int(len(list(entry)) ==  len(list(set(list(entry)))))
+        test2 = int(sorted(list(entry)) == list(entry) or sorted(list(entry), reverse=True) == list(entry))
+
+        output[entry_id] = np.prod(np.array(list(map(test, entry[:-1], entry[1:])))) * test1 * test2
+
+def main():
+    report = load_files()
+    out1 = task1(report)
+
+    return out1
 
 if __name__ == "__main__":
-    print(load_files())
+    print(main())
